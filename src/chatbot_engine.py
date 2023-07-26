a1a1094e47a75192515796bab6fa5996f31dd95b
sizesizecom <140306629+sizesizecom@users.noreply.github.com>
2023-07-26 22:39:12 +0900

    Add files via upload

---
+++ src/chatbot_engine.py
@@ -0,0 +1,21 @@
+import langchain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.memory import ChatMessageHistory
+
+langchain.verbose = True
+
+
+def create_index() -> VectorStoreIndexWrapper:
+    loader = DirectoryLoader("./src/", glob="**/*.py")
+    return VectorstoreIndexCreator().from_loaders([loader])
+
+
+def chat(
+    message: str, history: ChatMessageHistory, index: VectorStoreIndexWrapper
+) -> str:
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+
+    return index.query(question=message, llm=llm)
